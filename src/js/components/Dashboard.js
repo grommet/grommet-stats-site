f254ad95bb92b28f51b7c863f7bae99a677fa6a6
Gennetten <douglas.gennetten@hpe.com>
2017-11-10 14:11:10 -0700

    initial commit - sample app

---
+++ src/js/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Heading from 'grommet/components/Heading';
+import Paragraph from 'grommet/components/Paragraph';
+import Section from 'grommet/components/Section';
+
+export default () => (
+  <Section primary={true}>
+    <Heading tag='h2'>Stats Dashboard</Heading>
+    <Paragraph>This is where magic will happen...</Paragraph>
+  </Section>
+);
